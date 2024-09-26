e1e5dc2e2d60397e9c82e5eccffd85d2a0382713
M Gangadhar Reddy <126484126+gangadhar952@users.noreply.github.com>
2024-09-26 11:21:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const cardArray = [
+    {
+      name: 'fries',
+      img: './fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: './cheeseburger.png'
+    },
+    {
+      name: 'ice-cream',
+      img: './ice-cream.png'
+    },
+    {
+      name: 'pizza',
+      img: './pizza.png'
+    },
+    {
+      name: 'milkshake',
+      img: './milkshake.png'
+    },
+    {
+      name: 'hotdog',
+      img: './hotdog.png'
+    },
+    {
+        name: 'fries',
+        img: './fries.png'
+      },
+      {
+        name: 'cheeseburger',
+        img: './cheeseburger.png'
+      },
+      {
+        name: 'ice-cream',
+        img: './ice-cream.png'
+      },
+      {
+        name: 'pizza',
+        img: './pizza.png'
+      },
+      {
+        name: 'milkshake',
+        img: './milkshake.png'
+      },
+      {
+        name: 'hotdog',
+        img: './hotdog.png'
+      },
+  ]
+
+  console.log(cardArray)
